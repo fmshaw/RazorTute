a91e6062ea4a579e1e4da204353fd37ecc8dbbbe
Fred Shaw <genericness@gmail.com>
2018-08-13 12:55:23 +1000

    Add migrations, remove unused tools

---
+++ Migrations/20180813021651_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorTute.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    captureDate = table.Column<DateTime>(nullable: false),
+                    resources = table.Column<string>(nullable: true),
+                    distance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Location");
+        }
+    }
+}
